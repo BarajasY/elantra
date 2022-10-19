2431c5fe298a5b64d1ad5acf72fa5cd7153e678a
Yahir Moreno <yahirmb7568@gmail.com>
2022-10-18 23:32:51 -0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Carousel } from "./Carousel/Carousel";
 export { default as Details } from "./Details/Details"
 export { default as NACTOY } from "./NACTOY/NACTOY";
-export { default as Specifications } from "./Specifications/Specifications";
 export { default as Versions } from './Versions/Versions';
